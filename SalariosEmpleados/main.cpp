874f8f1321c07582e2461037002532e721ad1c1c
Carlos Rojas <carloseduardorojas1@javerianacali.edu.co>
2026-05-13 21:22:22 -0500

    Actividad proyecto POO con comentarios

--- SalariosEmpleados/main.cpp
+++ SalariosEmpleados/main.cpp
@@ -5,10 +5,10 @@ using std::cout;
 using std::cin;
 
 int main() {
-    Nomina nomina;
+    Nomina nomina; //Creo el objeto
     int opcion = 0;
 
-    while (opcion != 3){
+    while (opcion != 3){ // pongo las opciones del menu
         cout << "1. Mostrar empleados\n";
         cout << "2. Calcular nomina total\n";
         cout << "3. Salir\n";
@@ -18,7 +18,7 @@ int main() {
 
         switch(opcion){
 
-        case 1:
+        case 1: // Asigno la accion a cada opcion del switche
             nomina.imprimirNomina();
             break;
         case 2:
